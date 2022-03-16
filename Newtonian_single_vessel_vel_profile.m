0145bfcab7189a40770dd0aacc092b6e057bdb1c
AndreeaAlecu19 <81109043+AndreeaAlecu19@users.noreply.github.com>
2022-03-16 18:11:18 +0000

    Add files via upload

--- Newtonian_single_vessel_vel_profile.m
+++ Newtonian_single_vessel_vel_profile.m
@@ -6,17 +6,17 @@ set(0,'defaulttextfontsize');
 %% 1. Newtonian flow in a single cylindrical channel (Poiseuille velocity profile)
 %Equation: u(y)=G/(2mu)(a^2-y^2), which in polar coordinates is
 %u(r)=G/(2mu)(a^2-r^2)=deltaP/(2mu L)*(a^2-r^2)
-a=0.08;
+a=15;
 %However, our maximum velocity is : delta P/(4muL)a^2. so we must have a
 %pressure drop in order for the velocity to occur
 %set pressure drop:
-P1=10;
-P2m=60;
+P1=11000;
+P2m=1000;
 %Need vmax to be positive, so:
 deltaP=abs(P1-P2m);
-L=1;
+L=13000;
 G=deltaP/L;
-mu=5.008e-4; % Viscosity of blood is roughly 5 times more than the viscosity of water, viscosity of water is 1.0016 milipascals x second
+mu=5e-4; % Viscosity of blood is roughly 5 times more than the viscosity of water, viscosity of water is 1.0016 milipascals x second
 n=100; 
 r=linspace(-a,a,n);
 
